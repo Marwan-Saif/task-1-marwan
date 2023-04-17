26a2faca6ef901b91caa96106788a3b65b12ecaa
Your Name <you@example.com>
2023-04-18 00:06:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const students = [
+  { name: 'Marwan Saif', age: 21, major: 'Computer Science' },
+  { name: 'Mohamed Saif', age: 24, major: 'Engineer' },
+  { name: 'Moaz Saif', age: 16, major: 'student' }
+];
+
+app.get('/students', (req, res) => {
+  let studentList = '<ul>';
+
+  students.forEach(student => {
+    const { name, age, major } = student;
+    studentList += `<li>${name}, ${age}, ${major}</li>`;
+  });
+
+  studentList += '</ul>';
+
+  res.send(studentList);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
